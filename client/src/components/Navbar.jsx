6366f58975f1eaa99eaf73f20f9c9b1f181de0ad
Yusuf Mustahan <yumustyology@gmail.com>
2023-03-15 11:42:11 -0700

    updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,6 +10,7 @@ import { RiExchangeDollarLine } from "react-icons/ri";
 import { TransactionContext } from "../context/TransactionContext";
 import { shortenAddress } from "../utils/shortenAddress";
 import { Link } from "react-router-dom";
+import { HiCurrencyYen } from "react-icons/hi";
 
 const NavbarItem = ({ title, classProps, icon, anchor }) => {
   return (
@@ -31,62 +32,56 @@ const Navbar = () => {
     isLoading,
   } = useContext(TransactionContext);
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [navItems, setNavItems] = useState([
+    {
+      title: "Ecommerce",
+      anchor: "/",
+      icon: (
+        <BsFillBasketFill
+          fontSize={20}
+          className="text-white cursor-pointer mb-1"
+        />
+      ),
+    },
+    {
+      title: "Stake",
+      anchor: "/",
+      icon: (
+        <FaPiggyBank fontSize={20} className="text-white  cursor-pointer" />
+      ),
+    },
+    {
+      title: "Games",
+      icon: <FaGamepad fontSize={20} className="text-white  cursor-pointer" />,
+    },
+    {
+      title: "Exchange",
+      anchor: "#exchange",
+      icon: (
+        <RiExchangeDollarLine
+          fontSize={20}
+          className="text-white  cursor-pointer"
+        />
+      ),
+    },
+    {
+      title: "NFT Marketplace",
+      anchor: "/nft_page  ",
+      icon: (
+        <BsShop fontSize={20} className="text-white  cursor-pointer mb-1" />
+      ),
+    },
+  ]);
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <Link to='/'>
-        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        <Link to="/" className="flex items-center text-2xl font-bold">
+          {/* <img src={logo} alt="logo" className="w-32 cursor-pointer" /> */}
+          <HiCurrencyYen fontSize={35} color="#fff" /> &nbsp; YTK DAAP
         </Link>
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {[
-          {
-            title: "Ecommerce",
-            anchor: "/",
-            icon: (
-              <BsFillBasketFill
-                fontSize={20}
-                className="text-white cursor-pointer mb-1"
-              />
-            ),
-          },
-          {
-            title: "Stake",
-            anchor: "/",
-            icon: (
-              <FaPiggyBank
-                fontSize={20}
-                className="text-white  cursor-pointer"
-              />
-            ),
-          },
-          {
-            title: "Games",
-            icon: (
-              <FaGamepad fontSize={20} className="text-white  cursor-pointer" />
-            ),
-          },
-          {
-            title: "Exchange",
-            anchor: "#exchange",
-            icon: (
-              <RiExchangeDollarLine
-                fontSize={20}
-                className="text-white  cursor-pointer"
-              />
-            ),
-          },
-          {
-            title: "NFT Marketplace",
-            anchor: "/nft_page  ",
-            icon: (
-              <BsShop
-                fontSize={20}
-                className="text-white  cursor-pointer mb-1"
-              />
-            ),
-          },
-        ].map((item, index) => (
+        {navItems.map((item, index) => (
           <NavbarItem
             key={item + index}
             title={item.title}
@@ -127,21 +122,21 @@ const Navbar = () => {
         )}
         {toggleMenu && (
           <ul
-            className="z-10 fixed top-0 right-0 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+            className="z-10 fixed top-0 right-0 p-3 w-[100vw] h-screen shadow-2xl md:hidden list-none
+            flex flex-col justify-start items-start rounded-md blue-glassmorphism text-white animate-slide-in"
           >
-            <li className="text-xl w-full my-2">
+            <li className="text-xl w-full my-2 flex justify-end">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
-            {["Market", "Exchange", "Tutorials", "Wallets"].map(
-              (item, index) => (
-                <NavbarItem
-                  key={item + index}
-                  title={item}
-                  classProps="my-2 text-lg"
-                />
-              )
-            )}
+            {navItems.map((item, index) => (
+          <NavbarItem
+            key={item + index}
+            title={item.title}
+            icon={item.icon}
+            anchor={item.anchor}
+            classProps="my-2 text-lg" 
+          />
+        ))}
           </ul>
         )}
       </div>
---
+++ client/src/components/Untitled-2.js
@@ -0,0 +1,4 @@
+
+
+
+it's a image ipfs endpoint from a protected url how do i display the image
